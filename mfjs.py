b05b8f7ffb9cdc0e0d8bc14b5676d0a9e37c3b76
renmu123 <1101022351@qq.com>
2018-03-06 20:39:48 +0800

    更新

--- mfjs.py
+++ mfjs.py
@@ -9,11 +9,11 @@ def jhw(url, origin_price):
     item_title = soup.find('h2', id='item_title').get_text()
     rmb_price = soup.find_all('span', color='orange')[1].get_text()
     text = '商品名{}\n原价是{}\n现价是{}\n便宜了{}'.format(item_title, origin_price, rmb_price, origin_price - int(rmb_price))
-
-    mail_fyx = my_mail(text, '1101022351@qq.com')
-    mail_fyx.run()
+    if origin_price>int(rmb_price):
+        mail_fyx = my_mail(text, '1101022351@qq.com')
+        mail_fyx.run()
 
 
 if __name__ == '__main__':
-    jhw('http://surugaya.masadora.net/product/detail/602115592001', 68)
-
+    jhw('http://surugaya.masadora.net/product/detail/602002954001', 88)
+    jhw('http://surugaya.masadora.net/product/detail/120045357001', 23)
